7e7715f99b83b6a3d54bd52c9b59f5c49c345e38
Therenegadee <melkindaniil@yandex.ru>
2023-10-29 19:07:14 +0300

    fixed errors

--- src/main/java/parser/userService/mappers/jdbc/UserRowMapper.java
+++ src/main/java/parser/userService/mappers/jdbc/UserRowMapper.java
@@ -1,12 +1,14 @@
 package parser.userService.mappers.jdbc;
 
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 import parser.userService.models.User;
 import parser.userService.models.enums.ActivationStatus;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 
+@Component
 public class UserRowMapper implements RowMapper<User> {
     @Override
     public User mapRow(ResultSet rs, int rowNum) throws SQLException {
--- src/main/java/parser/userService/mappers/openapi/RoleMapper.java
+++ src/main/java/parser/userService/mappers/openapi/RoleMapper.java
@@ -1,11 +1,23 @@
 package parser.userService.mappers.openapi;
 
+import org.mapstruct.Mapper;
+import org.mapstruct.ValueMapping;
+import org.mapstruct.ValueMappings;
 import parser.userService.models.enums.ERole;
 import user.openapi.model.ERoleOpenApi;
 
+@Mapper(componentModel = "spring")
 public interface RoleMapper {
 
+    @ValueMappings({
+            @ValueMapping(source = "ROLE_USER", target = "USER"),
+            @ValueMapping(source = "ROLE_ADMIN", target = "ADMIN")
+    })
     ERoleOpenApi toOpenApi(ERole eRole);
 
+    @ValueMappings({
+            @ValueMapping(source = "USER", target = "ROLE_USER"),
+            @ValueMapping(source = "ADMIN", target = "ROLE_ADMIN")
+    })
     ERole toERole(ERoleOpenApi eRoleOpenApi);
 }
